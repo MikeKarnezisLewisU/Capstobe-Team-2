fb22a30020b54433e078b91c2c00f95ac0d377f6
Jack Krejci <jkrejci1988@gmail.com>
2022-09-21 15:17:37 -0500

    Created an error detection process for when signing up with invalid email, password, or an email that already exists.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,39 @@
 //Import the user model
 const User = require('../models/User')
 
+//Function to handle errors
+const handleErrors = (err) => {
+    //The err.message will be the custom error messages we made in the User schema!
+    console.log(err.message, err.code) //Provides the error type and its code if not unique
+
+    //Create and errors object
+    let errors = { email: '', password: '' }
+
+    //Whenever there's an error there's the message "user validation failed"
+    //So let's look for that phrase and if we have it we can mess around with it
+    
+    //Duplication errors (11000 is the error code for duplications)
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered!'
+        return errors;
+    }
+
+    //Validation errors
+    if (err.message.includes('user validation failed')) {
+        //console.log(err) //Object gives us error value of what the user tried to use, properties, path, kind of error inside the errors property
+        //console.log(Object.values(err.errors)) //Get the values of the different things inside the errors object; given as an array
+        //Lets get the individual errors
+        Object.values(err.errors).forEach(({properties}) => {
+            //console.log(properties)
+
+            //The path should either be an email or password
+            errors[properties.path] = properties.message //The properties.path will be either email or password so it will equal the value in our errors object and set them equal to the message for the corresponding error
+        })
+    }
+
+    return errors; //Send the errors object back
+} 
+
 //Functions to handle the get and post requests from the 'routes' folder
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -26,8 +59,11 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user) //Send back as json
     }
     catch(err) {
-        console.log(err); //Log the error
-        res.status(400).send('error, user not created') //Log the error type in the console
+        //console.log(err); //Log the error
+        //Use the handleErrors function to get correct error messages ready
+        const errors = handleErrors(err)
+        //res.status(400).send('error, user not created') //Log the error type in the console
+        res.status(400).json( { errors }) //Will respond with the errors and the error message for each error in json format
     }
     res.send('new signup');
 }
