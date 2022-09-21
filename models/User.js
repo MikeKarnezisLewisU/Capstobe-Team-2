cd17bc861cbdc691894c12820d95daa324c99b23
Jack Krejci <jkrejci1988@gmail.com>
2022-09-21 14:47:03 -0500

    Added the 'validator' package which will be used to validate if a user entered a valid email for our schema.

--- models/User.js
+++ models/User.js
@@ -2,22 +2,26 @@
 //Use mongoose to do this
 const mongoose = require('mongoose');
 
+//Use the validator package to validate emails
+const { isEmail } = require('validator') //Just need to pass this into validate now otherwise we'd have to use reqular expressions to check it
+
 //Create a schema for what our objects will look in the database
 const userSchema = new mongoose.Schema({
     
     //Require an email that will be a String and it must be unique from any other email on the site
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email!'], //Use array format to make customized error messages
         unique: true,
-        lowercase: true //Turns everything into lowercase when stored in DB
+        lowercase: true, //Turns everything into lowercase when stored in DB
+        validate: [isEmail, 'Please enter a valid email!']
     },
 
     //Now make it to where we will need a required password with a minimum of 6 characters
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password!'],
+        minlength: [6, 'Minimum password length is 6 characters!']
     },
 });
 
